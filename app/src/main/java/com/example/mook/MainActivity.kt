0a06ca43ef69ee6ff2eb8c6f6b698d904a98d57a
Michael Cardoso <Nuxglwk.Xlesiai@gmail.com>
2023-11-29 03:21:58 -0600

    updated Add books and Library page

--- app/src/main/java/com/example/mook/MainActivity.kt
+++ app/src/main/java/com/example/mook/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.mook
 
+import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -11,6 +13,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -18,12 +21,13 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Send
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.Card
 import androidx.compose.material3.Divider
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
@@ -51,19 +55,25 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
-import androidx.compose.ui.window.DialogProperties
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import coil.compose.AsyncImage
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import com.example.mook.helper.Book
 import com.example.mook.ui.theme.MookTheme
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
+import okhttp3.Headers
+import kotlin.system.exitProcess
 
 class MainActivity : ComponentActivity() {
 private lateinit var library: SharedPreferences
+private lateinit var context: Context
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -168,6 +178,7 @@ fun Library(library: SharedPreferences, navController: NavHostController) {
             var isAddClicked by remember{ mutableStateOf(false)}
             var isEditable by remember{ mutableStateOf(false)}
             val list = remember{ mutableStateListOf(library.all) }
+
             LazyVerticalGrid(
                 columns = GridCells.Adaptive(3.dp),
                 modifier = Modifier
@@ -178,10 +189,10 @@ fun Library(library: SharedPreferences, navController: NavHostController) {
                         bottom.linkTo(parent.bottom)
                     }
                     .fillMaxSize()
-                    .background(Color.Green)
+                    .background(Color(0, 255, 0, 20))
             ){
                 // Display User's Library
-                items(list.size){it ->
+                items(list.size){
                     Text(it.toString())
                 }
             }
@@ -261,16 +272,25 @@ fun Settings() {
 }
 @Composable
 fun AddBooks() {
+    val searchResults = remember { mutableStateListOf(Book())}
+    var title by remember{ mutableStateOf("") }
+    var author by remember{ mutableStateOf("") }
+    val client = AsyncHttpClient()
+
     ConstraintLayout(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
     ) {
         val (
             searchTitleText, searchTitleBox,
             searchAuthorText, searchAuthorBox,
             enterButton, results
         ) = createRefs()
+
         Text(
             text = "Title: ",
+            color = MaterialTheme.colorScheme.inversePrimary,
             modifier = Modifier
                 .constrainAs(searchTitleText){
                     top.linkTo(parent.top)
@@ -280,35 +300,131 @@ fun AddBooks() {
                 }
         )
         TextField(
-            value = "Title",
-            onValueChange = {},
+            value = title,
+            onValueChange = {
+                 title = it
+            },
             singleLine = true,
             modifier = Modifier
-                .constrainAs(searchTitleBox){
-                    top.linkTo(parent.top)
-                    end.linkTo(parent.end)
+                .constrainAs(searchTitleBox) {
+                    top.linkTo(parent.top, 16.dp)
+                    end.linkTo(parent.end, 16.dp)
                 }
+                .clip(RoundedCornerShape(50))
+
         )
         Text(
             text = "Author: ",
+            color = MaterialTheme.colorScheme.inversePrimary,
             modifier = Modifier
                 .constrainAs(searchAuthorText){
-                    top.linkTo(searchTitleText.bottom)
+                    top.linkTo(searchAuthorBox.top)
+                    bottom.linkTo(searchAuthorBox.bottom)
                     start.linkTo(searchTitleText.start)
 
             }
         )
         TextField(
-            value = "Author",
-            onValueChange = {},
+            value = author,
+            onValueChange = {
+                author = it
+            },
             singleLine = true, modifier = Modifier
-                .constrainAs(searchAuthorBox){
-                    top.linkTo(searchTitleBox.bottom)
+                .constrainAs(searchAuthorBox) {
+                    top.linkTo(searchTitleBox.bottom, 16.dp)
                     end.linkTo(searchTitleBox.end)
 
                 }
+                .clip(RoundedCornerShape(50))
+
 
         )
+        LazyColumn(
+            modifier = Modifier
+                .constrainAs(results) {
+                    top.linkTo(searchAuthorBox.bottom, 16.dp)
+                    start.linkTo(parent.start, 10.dp)
+                    end.linkTo(parent.end, 10.dp)
+                    bottom.linkTo(enterButton.bottom)
+
+                    width = Dimension.fillToConstraints
+                    height = Dimension.fillToConstraints
+                }
+                .clip(RoundedCornerShape(10))
+                .background(Color(0, 0, 0, 50))
+
+        ){
+            items(searchResults.size){
+                SearchBookResult(book = searchResults[it])
+            }
+        }
+        IconButton(
+            onClick = {
+                Log.d("Send Button", "Title: $title, Author: $author")
+                searchResults.clear()
+
+                val new_title = title.replace(" ", "+")
+                val new_author = author.replace(" ", "+")
+                var link = "https://openlibrary.org/search.json?title=$new_title&author=$new_author&fields=title,author_name,cover_edition_key"
+                if (new_title.isBlank())
+                    link = "https://openlibrary.org/search.json?author=$new_author&fields=title,author_name,cover_edition_key"
+                if (new_author.isBlank())
+                    link = "https://openlibrary.org/search.json?title=$new_title&fields=title,author_name,cover_edition_key"
+                if (new_author.isBlank() and new_title.isBlank())
+                    exitProcess(0)
+
+                client[link,
+                    object: JsonHttpResponseHandler(){
+                    override fun onFailure(
+                        statusCode: Int,
+                        headers: Headers?,
+                        response: String?,
+                        throwable: Throwable?
+                    ) {
+
+                    }
+
+                    override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON?) {
+                        json?.jsonObject?.getJSONArray("docs")?.let { docsArray ->
+                            for (i in 0 until docsArray.length()) {
+                                val obj = docsArray.getJSONObject(i)
+                                try {
+                                    val book = Book(
+                                        obj.getString("title"),
+                                        obj.getJSONArray("author_name")[0].toString(),
+                                        "https://covers.openlibrary.org/a/olid/${obj.getString("cover_edition_key")}-M.jpg"
+                                    )
+                                    searchResults.add(book)
+                                }
+                                catch (e:Exception){
+                                    Log.e("OpenLibrary", e.toString())
+                                }
+
+
+                            }
+                        }
+                    }
+
+                }]
+            },
+            modifier = Modifier
+                .padding(30.dp)
+                .clip(CircleShape)
+                .background(MaterialTheme.colorScheme.primary)
+                .constrainAs(enterButton) {
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                    bottom.linkTo(parent.bottom, 16.dp)
+
+                }
+
+        ) {
+            Icon(
+                imageVector = Icons.Default.Send,
+                contentDescription = null
+
+            )
+        }
     }
 }
 // ---------------------------Dialogs---------------------------
@@ -346,6 +462,7 @@ fun LibraryBook() {
                     end.linkTo(parent.end)
                 }
                 .background(Color.Blue)
+
         )
         Text(
             text = "Author",
@@ -362,12 +479,52 @@ fun LibraryBook() {
     }
 }
 
+@Composable
+fun SearchBookResult(book: Book) {
+    ConstraintLayout (
+        modifier = Modifier
+            .height(100.dp)
+            .fillMaxWidth()
+    ){
+        val (image, title, author) = createRefs()
+        AsyncImage(
+            model = book.cover,
+            contentDescription = null,
+            modifier = Modifier
+                .constrainAs(image) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    bottom.linkTo(parent.bottom)
+                }
+                .width(70.dp)
+        )
+        Text(
+            text = book.title,
+            modifier = Modifier
+                .constrainAs(title){
+                    top.linkTo(parent.top)
+                    start.linkTo(image.end, 16.dp)
+                    bottom.linkTo(author.top)
+                }
+        )
+        Text(
+            text = book.author,
+            modifier = Modifier
+                .constrainAs(author){
+                    top.linkTo(title.bottom)
+                    start.linkTo(image.end, 32.dp)
+                    bottom.linkTo(parent.bottom)
+                }
+        )
+    }
+    
+}
 
 // ---------------------------Preview---------------------------
 @Preview(showBackground = true)
 @Composable
 fun prev() {
     MookTheme {
-        AddBooks()
+        SearchBookResult(Book("Harry Potter", "J.K Rowling", "https://covers.openlibrary.org/a/olid/OL23919A-M.jpg"))
     }
 }
--- app/src/main/java/com/example/mook/helper/Book.kt
+++ app/src/main/java/com/example/mook/helper/Book.kt
@@ -1,6 +1,9 @@
 package com.example.mook.helper
 
 class Book(val title: String = "", val author: String = "", val cover: String = "", val text: String = "", val audio: String = "") {
+    companion object{
+
+    }
     override fun toString(): String {
         return "Title: ${title}| Author: ${author}| Cover: ${cover}| Text: ${text}| Audio: ${audio}|"
     }
